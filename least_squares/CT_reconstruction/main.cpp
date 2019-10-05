3b1296707baac065e10acd37c6fd2f3caa4d4e98
yimu <yimuw92@gmail.com>
2019-10-04 22:40:40 -0400

    ct-recons: compiled

---
+++ least_squares/CT_reconstruction/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "ct_reconstruction.h"
+
+
+int main(int argc, char *argv[])
+{
+    CtReconstruction ct_reconstruction;
+    ct_reconstruction.ct_reconstruction_simulation();
+
+    return 0;
+}
