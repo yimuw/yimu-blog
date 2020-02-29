b703e6e9cb722f797a4862fa65beef99a7213ffe
yimu <yimuw92@gmail.com>
2020-02-28 20:36:39 -0500

    dehaze: without smoothing

---
+++ on_the_shoulders/he_kaiming/dehaze/dehaze.h
@@ -0,0 +1,180 @@
+#pragma once
+
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+
+class Dehaze
+{
+public:
+    struct Config
+    {
+    };
+
+    using index_and_intensity = std::pair<cv::Point2d, float>;
+
+    Dehaze() = default;
+
+    void dehaze(const cv::Mat &haze_im)
+    {
+        const cv::Mat dark_channel = find_dark_channel(haze_im, 5);
+        const cv::Vec3f atmospheric_light = compute_atmospheric_light(dark_channel, haze_im);
+        cv::Mat t = compute_t(haze_im, atmospheric_light);
+        recover_J(haze_im, t, atmospheric_light);
+    }
+
+    cv::Mat recover_J(const cv::Mat &haze_im_in, const cv::Mat &t_in, const cv::Vec3f &atmospheric_light)
+    {
+        cv::Mat t = t_in.clone();
+        cv::Mat haze_im = haze_im_in.clone();
+        constexpr float MIN_T = 0.01;
+
+        t.setTo(MIN_T, t < MIN_T);
+
+        cv::Mat J(t.rows, t.cols, CV_32FC3);
+
+        for(int row_idx = 0; row_idx < t.rows; ++row_idx)
+        {
+            for(int col_idx = 0; col_idx < t.cols; ++col_idx)
+            {
+                cv::Vec3f haze_bgr = haze_im.at<cv::Vec3f>(row_idx, col_idx);
+                float robust_t = std::max(t.at<float>(row_idx, col_idx), MIN_T);
+                J.at<cv::Vec3f>(row_idx, col_idx) =                
+                    (haze_bgr - atmospheric_light) / robust_t + atmospheric_light;
+            }
+        }
+
+        cv::imshow("J", J);
+        cv::waitKey();
+
+        return J;
+    }
+
+    cv::Mat compute_t(const cv::Mat &haze_im, const cv::Vec3f &atmospheric_light)
+    {
+        cv::Mat bgr[3];
+        cv::split(haze_im, bgr);
+        bgr[0] = bgr[0] / atmospheric_light[0];
+        bgr[1] = bgr[1] / atmospheric_light[1];
+        bgr[2] = bgr[2] / atmospheric_light[2];
+
+        cv::Mat normalized;
+        cv::merge(bgr, 3, normalized);
+
+        constexpr float OMAGE = 0.95;
+        const cv::Mat t = 1 - OMAGE * find_dark_channel(normalized, 3);
+        cv::imshow("t map", t);
+        cv::waitKey();
+        return t;
+    }
+
+    cv::Mat find_dark_channel(const cv::Mat &m, const int window_half_size)
+    {
+
+        cv::Mat m_pad;
+        cv::copyMakeBorder(m, m_pad, window_half_size, window_half_size,
+               window_half_size, window_half_size, cv::BORDER_REPLICATE);
+
+        if (false) {
+            cv::imshow("m_pad", m_pad);
+            cv::waitKey(0);
+        }
+
+        cv::Mat dark_chaneel = cv::Mat(m.rows, m.cols, CV_32FC1, 0.);
+
+        for(int row_idx = window_half_size; row_idx < m_pad.rows - window_half_size; ++row_idx)
+        {
+            for(int col_idx = window_half_size; col_idx < m_pad.cols - window_half_size; ++col_idx)
+            {
+                dark_chaneel.at<float>(row_idx - window_half_size, col_idx - window_half_size) = find_min_in_rbg_patch(m_pad, row_idx, col_idx, window_half_size);
+            }
+        }
+
+        if (false) {
+            cv::imshow("dark_chaneel", dark_chaneel);
+            cv::waitKey(0);
+        }
+
+        return dark_chaneel;
+    }
+
+    inline float find_min_in_rbg_patch(const cv::Mat &m, const int row, const int col, const int window_half_size)
+    {
+        float min_val = std::numeric_limits<float>::max();
+        for(int dy = - window_half_size; dy <= window_half_size; ++dy)
+        {
+            for(int dx = - window_half_size; dx <= window_half_size; ++ dx)
+            {
+                const cv::Vec3f gbr = m.at<cv::Vec3f>(row + dy, col + dx);
+                min_val = std::min(gbr[0], min_val);
+                min_val = std::min(gbr[1], min_val);
+                min_val = std::min(gbr[2], min_val);
+            }
+        }
+        return min_val;
+    }
+
+    cv::Vec3f compute_atmospheric_light(const cv::Mat &dark_channel, const cv::Mat &haze_im)
+    {
+        std::vector<index_and_intensity> dark_channel_index_and_intensities;
+
+        for(int row_idx = 0; row_idx < dark_channel.rows; ++row_idx)
+        {
+            for(int col_idx = 0; col_idx < dark_channel.cols; ++col_idx)
+            {
+                float intensity = dark_channel.at<float>(row_idx, col_idx );
+                dark_channel_index_and_intensities.emplace_back(
+                    cv::Point2d(col_idx, row_idx), intensity);
+            }
+        }
+
+        std::sort(dark_channel_index_and_intensities.begin(), dark_channel_index_and_intensities.end(),
+        [](const index_and_intensity &l, const index_and_intensity &r){
+            return l.second < r.second;
+        });
+
+        std::vector<index_and_intensity> candidate_from_dark(
+            dark_channel_index_and_intensities.end() - dark_channel_index_and_intensities.size() * 0.001,
+            dark_channel_index_and_intensities.end());    
+
+        std::cout << "candidate_from_dark.size():" << candidate_from_dark.size() << std::endl;
+
+        // show_pixel(haze_im, candidate_from_dark, "candidate_from_dark");
+
+        std::vector<index_and_intensity> index_and_im_intensity;
+        for(const auto &p : candidate_from_dark)
+        {
+            const cv::Vec3f bgr= haze_im.at<cv::Vec3f>(p.first);
+            const float B = bgr[0];
+            const float G = bgr[1];
+            const float R = bgr[2];
+            const float intensity = ( (0.3 * R) + (0.59 * G) + (0.11 * B) );
+            index_and_im_intensity.emplace_back(p.first, intensity);
+        }
+
+        std::sort(index_and_im_intensity.begin(), index_and_im_intensity.end(),
+        [](const index_and_intensity &l, const index_and_intensity &r){
+            return l.second < r.second;
+        });
+
+        // show_pixel(haze_im, {index_and_im_intensity.back()}, "highest intensity");
+
+        cv::Point2d atmospheric_light_idx = index_and_im_intensity.back().first;
+        return haze_im.at<cv::Vec3f>(atmospheric_light_idx);
+    }
+
+    void show_pixel(const cv::Mat &im, 
+                    const std::vector<index_and_intensity> &candidate_from_dark,
+                    const std::string &info)
+    {
+        cv::Mat im_viz = im.clone();
+        for(const auto &p : candidate_from_dark)
+        {
+            im_viz.at<cv::Vec3f>(p.first) = cv::Vec3f(0, 0, 1.);
+        }
+
+        cv::imshow(info, im_viz);
+        cv::waitKey();
+    }
+    
+};
---
+++ on_the_shoulders/he_kaiming/dehaze/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "dehaze.h"
+
+
+int main(int argc, char* argv[])
+{
+    cv::Mat haze_im = cv::imread("/home/yimu/Desktop/blog/yimu-blog/on_the_shoulders/he_kaiming/dehaze/forest1.jpg");
+    haze_im.convertTo(haze_im, CV_32FC3, 1/255.0);
+
+    Dehaze dehaze;
+
+    // cv::imshow("m_pad", haze_im);
+    // cv::waitKey(0);
+
+    dehaze.dehaze(haze_im);
+
+    return 0;
+}
