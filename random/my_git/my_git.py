9c1e96f359f0dce24f15ffb9de0d279d1578d688
yimu <yimuw92@gmail.com>
2020-11-11 19:47:54 -0500

    my git: decoupe data and tree node; working

--- random/my_git/my_git.py
+++ random/my_git/my_git.py
@@ -10,16 +10,16 @@ join = os.path.join
 
 
 class TreeNode:
-    def __init__(self, file_path, message, new_file, parents=[]):
-        self.file_path = file_path
-        self.message = message
-        self.new_file = new_file
+    def __init__(self, id, blobs=[], parents=[]):
+        self.id = id
+        self.blobs = blobs
         self.parents = parents
         self.children = []
-        self.file_content = None
-        self.hash_val = None
 
-    def compute_hash(self):
+
+class Blob:
+    def __init__(self, file_path):
+        self.file_path = file_path
         self.file_content = open(self.file_path, 'r').read()
         self.hash_val = hashlib.sha1(
             self.file_content.encode('utf-8')).hexdigest()
@@ -33,7 +33,8 @@ class MyGit:
 
     def __init__(self, dir='.'):
         self.git_dir = 'my_git'
-        self.objects_dir = join(dir, self.git_dir, 'objects')
+        self.blobs_dir = join(dir, self.git_dir, 'blobs')
+        self.nodes_dir = join(dir, self.git_dir, 'nodes')
         self.states_path = join(dir, self.git_dir, 'states')
         self.states = self.States()
         self.root_name = 'ROOT'
@@ -43,13 +44,12 @@ class MyGit:
             print('git dir exist!')
             return
         os.mkdir(self.git_dir)
-        os.mkdir(self.objects_dir)
+        os.mkdir(self.blobs_dir)
+        os.mkdir(self.nodes_dir)
 
-        root = TreeNode(self.root_name, 'root', True)
-        root.hash_val = self.root_name
-        root.file_content = ''
+        root = TreeNode(self.root_name)
 
-        self.states.head = self.root_name
+        self.states.head = root.id
 
         self.save_node(root)
         self.save_states()
@@ -62,138 +62,159 @@ class MyGit:
     def save_states(self):
         pickle.dump(self.states, open(join(self.git_dir, 'states'), 'wb'))
 
-    def load_node(self, hash):
-        return pickle.load(open(join(self.objects_dir, hash), 'rb'))
+    def load_node(self, node_id):
+        return pickle.load(open(join(self.nodes_dir, node_id), 'rb'))
 
     def save_node(self, node):
-        pickle.dump(node, open(join(self.objects_dir, node.hash_val), 'wb'))
+        pickle.dump(node, open(join(self.nodes_dir, node.id), 'wb'))
+
+    def load_blob(self, blob_hash):
+        return pickle.load(open(join(self.blobs_dir, blob_hash), 'rb'))
+
+    def save_blob(self, blob):
+        pickle.dump(blob, open(join(self.blobs_dir, blob.hash_val), 'wb'))
 
     def status(self):
         self.load_states()
         print('HEAD: ', self.states.head)
         print('tracked files:', self.states.tracked_files)
+        for f in self.states.tracked_files:
+            print('content of f:', f)
+            print(open(f, 'r').read())
 
-    def commit(self, file_path, message):
+    def add(self, files):
+        self.load_states()
+        for f in files:
+            if os.path.exists(f):
+                self.states.tracked_files.add(f)
+            else:
+                print(f, 'does not exist!')
+        self.save_states()
+
+    def commit(self, message, verbose=True):
         self.load_states()
         print('current HEAD:', self.states.head)
 
-        if not os.path.exists(file_path):
-            print('file does not exist')
-            return
+        has_change = False
 
-        new_node = TreeNode(
-            file_path, message, new_file=file_path not in self.states.tracked_files, parents=[self.states.head])
-        new_node.compute_hash()
+        all_blob = []
+        for f in self.states.tracked_files:
+            blob = Blob(f)
+            all_blob.append(blob.hash_val)
+            if blob.hash_val not in os.listdir(self.blobs_dir):
+                has_change = True
+                self.save_blob(blob)
 
         # using hash to detect changes
-        if new_node.hash_val == self.states.head:
+        if has_change == False:
             print('nothing to commit!')
             return
 
+        new_node = TreeNode(message, blobs=all_blob,
+                            parents=[self.states.head])
+
         # update the children of current head node
         head_node = self.load_node(self.states.head)
-        head_node.children.append(new_node.hash_val)
+        head_node.children.append(new_node.id)
         self.save_node(head_node)
 
         # head point to new node
-        self.states.head = new_node.hash_val
-        self.states.tracked_files.add(file_path)
+        self.states.head = new_node.id
         self.save_states()
 
         # save the new node
         self.save_node(new_node)
+        if verbose:
+            print('commit success! ')
+            print('new HEAD:', self.states.head)
 
-        print('commit success! ')
-        print('new HEAD:', self.states.head)
+    def __merge_commit(self, p1, p2):
+        merge_node = TreeNode(
+            'merge-{}-{}'.format(p1[:6], p2[:6]), parents=[p1, p2])
+        merge_node.hash_val = 'merge-{}-{}'.format(p1[:6], p2[:6])
 
-    def __find_path_to_node(self, cur_node_hash, target_node_hash, path):
-        cur_node = self.load_node(cur_node_hash)
-        path.append(cur_node.hash_val)
+        # update the children of p1, p2 node
+        p1_node = self.load_node(p1)
+        p1_node.children.append(merge_node.hash_val)
+        self.save_node(p1_node)
 
-        if cur_node.hash_val == target_node_hash:
-            return True
+        p2_node = self.load_node(p2)
+        p2_node.children.append(merge_node.hash_val)
+        self.save_node(p2_node)
 
-        for c in cur_node.children:
-            if self.__find_path_to_node(c, target_node_hash, path):
-                return True
-
-        path.pop()
-        print(path)
-        return False
-
-    def __lowest_common_ancester_path(self, node_hash1, node_hash2):
-        print('finding LCA path between', node_hash1, node_hash2)
-        path1, path2 = [], []
-
-        if self.__find_path_to_node(self.root_name, node_hash1, path1) \
-           and self.__find_path_to_node(self.root_name, node_hash2, path2):
-            idx = 0
-            lca = None
-            for i in range(min(len(path1), len(path2))):
-                if path1[i] != path2[i]:
-                    break
-                idx = i
-                lca = path1[i]
-            return path1[idx:][::-1] + path2[idx+1:] , lca
-
-        else:
-            print('fail to find a LCA path')
-            return [], None
-
-    def checkout(self, id):
-        if id == self.root_name:
-            print('can not checkout virtual node root')
-            return
-
-        if id not in os.listdir(self.objects_dir):
-            print('commit doesn not exist')
-            return
-
-        print('checkout hash id...')
-        self.load_states()
-        path, _ = self.__lowest_common_ancester_path(self.states.head, id)
-        print('LCA path:', path)
-
-        for i in range(len(path) - 1):
-            cur_node_hash = path[i]
-            next_node_hash = path[i+1]
-
-            print('go to hash:', next_node_hash)
-            self.states.head = next_node_hash
-
-            cur_node = self.load_node(cur_node_hash)
-            if cur_node.new_file:
-                os.remove(cur_node.file_path)
-                self.states.tracked_files.remove(cur_node.file_path)
-
-            next_node = self.load_node(next_node_hash)
-            self.states.tracked_files.add(next_node.file_path)
-            with open(next_node.file_path, 'w') as file:
-                file.write(next_node.file_content)
-
-        assert(self.states.head == id)
+        # head point to new node
+        self.states.head = merge_node.hash_val
         self.save_states()
 
-    def merge(self, id):
-        if id == self.root_name:
-            print('can not merge virtual node root')
-            return
-        
-        if id not in os.listdir(self.objects_dir):
+        # save the new node
+        self.save_node(merge_node)
+
+        print('merged commit success! ')
+        print('new HEAD:', self.states.head)
+
+    def checkout(self, node_id, verbose=True):
+        if node_id not in os.listdir(self.nodes_dir):
             print('commit doesn not exist')
             return
 
         self.load_states()
-        path, lca = self.__lowest_common_ancester_path(self.states.head, id)
-        print('LCA path:', path)
-        print('LCA node:', lca)
 
-        if lca == id or lca == self.states.head:
-            print('fastforward...')
-            self.checkout(id)
+        for f in self.states.tracked_files:
+            os.remove(f)
+        self.states.tracked_files = set()
+
+        node = self.load_node(node_id)
+        for blob_hash in node.blobs:
+            blob = self.load_blob(blob_hash)
+            self.states.tracked_files.add(blob.file_path)
+            with open(blob.file_path, 'w') as file:
+                file.write(blob.file_content)
+        self.states.head = node_id
+        self.save_states()
+
+        if verbose:
+            print('checkout success!')
+            print('new HEAD:', self.states.head)
+
+    def __lowest_common_ancester_path(self, node1_id, node2_id):
+        print('finding LCA path between', node1_id, node2_id)
+
+        self.lca = None
+
+        def recursion(node_id):
+            node = self.load_node(node_id)
+
+            value = 0
+            for c in node.children:
+                value += recursion(c)
+
+            value += 1 if node.id == node1_id or node.id == node2_id else 0
+
+            if self.lca is None and value == 2:
+                self.lca = node.id
+            return value
+        recursion(self.root_name)
+
+        return self.lca
+
+    def __detect_diff3_conflict(self, path):
+        return '<<<<<<<' in open(path, 'r').read()
+
+    def merge(self, node_id):
+        if node_id not in os.listdir(self.nodes_dir):
+            print('commit doesn not exist')
             return
 
-        merge_id = 'merge-{}-{}'.format(self.states.head, id)
+        self.load_states()
+        lca = self.__lowest_common_ancester_path(self.states.head, node_id)
+        print('LCA node:', lca)
+
+        if lca == node_id or lca == self.states.head:
+            print('fastforward...')
+            self.checkout(node_id)
+            return
+
+        merge_id = 'merge-{}-{}'.format(self.states.head, node_id)
         merge_dir = join(self.git_dir, merge_id)
         source_dir = join(merge_dir, 'source')
         target_dir = join(merge_dir, 'target')
@@ -210,45 +231,25 @@ class MyGit:
             for f in self.states.tracked_files:
                 shutil.copyfile(f, join(dir, f))
 
-        # traverse the path and take snapshot for source, target and lca
+        cur_head = self.states.head
+
         snapshot(source_dir)
         source_files = copy.deepcopy(self.states.tracked_files)
 
-        cur_head = self.states.head
-        for i in range(len(path) - 1):
-            cur_node_hash = path[i]
-            next_node_hash = path[i+1]
+        self.checkout(lca, verbose=False)
+        snapshot(lca_dir)
+        lca_files = copy.deepcopy(self.states.tracked_files)
 
-            print('go to hash:', next_node_hash)
-            self.states.head = next_node_hash
-
-            cur_node = self.load_node(cur_node_hash)
-            if cur_node.new_file:
-                os.remove(cur_node.file_path)
-                self.states.tracked_files.remove(cur_node.file_path)
-
-            next_node = self.load_node(next_node_hash)
-            self.states.tracked_files.add(next_node.file_path)
-            with open(next_node.file_path, 'w') as file:
-                file.write(next_node.file_content)
-
-            # save lca files
-            if next_node_hash == lca:
-                snapshot(lca_dir)
-                lca_files = copy.deepcopy(self.states.tracked_files)
-        
-        assert(self.states.head == id)
+        self.checkout(node_id, verbose=False)
         snapshot(target_dir)
         target_files = copy.deepcopy(self.states.tracked_files)
 
-        # go back to original head
-        self.save_states()
-        self.checkout(cur_head)
+        self.checkout(cur_head, verbose=False)
 
         all_files = target_files.union(source_files).union(lca_files)
-        print(all_files)
+
         for f in all_files:
-            print('merging...', f)
+            print('...merging...', f)
             sf = join(source_dir, f)
             tf = join(target_dir, f)
             lf = join(lca_dir, f)
@@ -256,50 +257,56 @@ class MyGit:
             # 1. f in lca, source and target.
             # do a diff3
             if f in source_files and f in target_files and f in lca_files:
-                os.system('diff3 {} {} {} -m > {}'.format(sf,lf,tf, f))
+                os.system('diff3 {} {} {} -m > {}'.format(sf, lf, tf, f))
+                self.states.tracked_files.add(f)
                 print('3 way merge')
+                if self.__detect_diff3_conflict(f):
+                    print(
+                        '!!!! Merge conflict for {}!!!! please address in another commit!'.format(f))
             # 2. f in source and lca, but not target.
             # take source
             elif f in source_files and f in lca_files:
                 shutil.copyfile(sf, f)
+                self.states.tracked_files.add(f)
                 print('new change from source. take from source')
             # 3. f in target and lca, but not source.
             # take target
             elif f in target_files and f in lca_files:
                 shutil.copyfile(tf, f)
+                self.states.tracked_files.add(f)
                 print('new change from target. take from target')
             # 4. f in source or (in source and target).
             # new file. take source
             elif f in source_files:
                 shutil.copyfile(sf, f)
+                self.states.tracked_files.add(f)
                 print('new file. take from source')
             # 5. f only in target.
             # new file. take target
             elif f in target_files:
                 shutil.copyfile(tf, f)
+                self.states.tracked_files.add(f)
                 print('new file. take from target')
             # 6. f only in lca.
             # file is removed. Do nothing
             elif f in lca_files:
                 print('file is removed')
             else:
-                print('?')
+                print('I worte a bug!')
 
-
+        # create a merge commit which has 2 parents
+        self.__merge_commit(self.states.head, node_id)
 
     def log(self):
-        def tree_recursion(node_hash):
-            if node_hash is None:
+        def tree_recursion(node_id):
+            if node_id is None:
                 return
 
-            node = self.load_node(node_hash)
+            node = self.load_node(node_id)
 
             print('======================')
-            print('node hash: ', node.hash_val)
-            print('message:', node.message)
-            print('file path:', node.file_path)
-            print('new file:', node.new_file)
-            print('file content:', node.file_content)
+            print('node id/message: ', node.id)
+            print('blobs:', node.blobs)
             print('children: ', node.children)
             print('parents: ', node.parents)
             print('======================')
@@ -317,8 +324,8 @@ class MyGit:
         self.load_states()
 
         def node_info_str(node):
-            return '{} {}'.format(node.hash_val[:6], node.message)
-        
+            return '{}'.format(node.id)
+
         def get_edges(node_hash):
             node = self.load_node(node_hash)
 
@@ -352,13 +359,14 @@ if __name__ == '__main__':
     argsubparsers.add_parser('test')
     argsubparsers.add_parser('gitk')
 
+    argsp = argsubparsers.add_parser('add')
+    argsp.add_argument('files', nargs='+')
+
     argsp = argsubparsers.add_parser('merge')
     argsp.add_argument('id',
                        help='target commit')
 
     argsp = argsubparsers.add_parser('commit')
-    argsp.add_argument('file_path',
-                       help='file to commit')
     argsp.add_argument('message',
                        help='file to commit')
 
@@ -373,7 +381,7 @@ if __name__ == '__main__':
     elif args.command == 'checkout':
         git.checkout(args.id)
     elif args.command == 'commit':
-        git.commit(args.file_path, args.message)
+        git.commit(args.message)
     elif args.command == 'init':
         git.init()
     elif args.command == 'log':
@@ -382,3 +390,5 @@ if __name__ == '__main__':
         git.gitk()
     elif args.command == 'merge':
         git.merge(args.id)
+    elif args.command == 'add':
+        git.add(args.files)
